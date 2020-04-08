a917f2bdfca557d76048a369e72d1ab2e3b53182
mayur <mayurpande.uk@gmail.com>
2020-04-08 11:25:17 +0200

    moved code to function to assert row text of table td elements

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,10 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -41,14 +45,8 @@ class NewVisitorTest(unittest.TestCase):
             input_box.send_keys(Keys.ENTER) #3
             time.sleep(1) #4
 
-            table = self.browser.find_element_by_id('id_list_table')
-            rows = table.find_elements_by_tag_name('tr') #1
-            self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-            self.assertIn('2: Use peacock feather to make a fly', [row.text for row in rows])
-            # self.assertTrue(
-            #     any(row.text == '1: Buy peacock feather' for row in rows),
-            #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-            # )
+            self.check_for_row_in_list_table('1: Buy peacock feathers')
+            self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
             # There is still a text box inviting her to add another item. She
             # enters "Use peacock feathers to make a fly" (Edith is very methodical)
