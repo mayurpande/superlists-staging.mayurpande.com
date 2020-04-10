7fd871b207a442dd1c9447d5c1288dca011a6bf0
mayur <mayurpande.uk@gmail.com>
2020-04-10 19:28:50 +0200

    set STATIC_ROOT in settings and disabled admin

--- tdd_book/superlists/settings.py
+++ tdd_book/superlists/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -124,5 +124,6 @@ USE_TZ = True
 # ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
