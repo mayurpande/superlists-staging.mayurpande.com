d364c2ba62348dc0b7136cb74a587da0e840f530
mayur <mayurpande.uk@gmail.com>
2020-05-07 18:35:38 +0200

    created fab cmds in python to handle session

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .server_tools import reset_database
 import time
 import unittest
 import os
@@ -30,6 +31,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
