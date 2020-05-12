25fc9b19fc61c58e53374c10680121f0924c5248
mayur <mayurpande.uk@gmail.com>
2020-05-12 13:44:46 +0200

    add screenshot on failure to FT runner

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self):
-
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
